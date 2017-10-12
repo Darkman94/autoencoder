7f57b5436c8c07af0e25cd639d0b4c929948548c
Barton <george.barton@goodlifefitness.com>
2017-10-12 15:23:56 -0400

    Improvements to the Tensorflow model

--- encode_tf.py
+++ encode_tf.py
@@ -15,18 +15,38 @@ import tensorflow as tf
 x = tf.placeholder(tf.float32, [None, 784])
 
 #build our neural network
-W_1 = tf.Variable(tf.zeros([784,400]))
-b_1 = tf.Variable(tf.zeros([400]))
+W_1 = tf.Variable(tf.zeros([784,512]))
+b_1 = tf.Variable(tf.zeros([512]))
 
-W_2 = tf.Variable(tf.zeros([400,100]))
-b_2 = tf.Variable(tf.zeros([100]))
+W_2 = tf.Variable(tf.zeros([512,256]))
+b_2 = tf.Variable(tf.zeros([256]))
 
-W_3 = tf.Variable(tf.zeros([100,784]))
-b_3 = tf.Variable(tf.zeros([784]))
+W_3 = tf.Variable(tf.zeros([256,128]))
+b_3 = tf.Variable(tf.zeros([128]))
+
+W_4 = tf.Variable(tf.zeros([128,64]))
+b_4 = tf.Variable(tf.zeros([64]))
+
+W_5 = tf.Variable(tf.zeros([64,128]))
+b_5 = tf.Variable(tf.zeros([128]))
+
+W_6 = tf.Variable(tf.zeros([128,256]))
+b_6 = tf.Variable(tf.zeros([256]))
+
+W_7 = tf.Variable(tf.zeros([256,512]))
+b_7 = tf.Variable(tf.zeros([512]))
+
+W_8 = tf.Variable(tf.zeros([512,784]))
+b_8 = tf.Variable(tf.zeros([784]))
 
 y_1 = tf.nn.sigmoid(tf.matmul(x, W_1) + b_1)
 y_2 = tf.nn.sigmoid(tf.matmul(y_1, W_2) + b_2)
-y = tf.nn.sigmoid(tf.matmul(y_2, W_3) + b_3)
+y_3 = tf.nn.sigmoid(tf.matmul(y_2, W_3) + b_3)
+y_4 = tf.nn.sigmoid(tf.matmul(y_3, W_4) + b_4)
+y_5 = tf.nn.sigmoid(tf.matmul(y_4, W_5) + b_5)
+y_6 = tf.nn.sigmoid(tf.matmul(y_5, W_6) + b_6)
+y_7 = tf.nn.sigmoid(tf.matmul(y_6, W_7) + b_7)
+y = tf.nn.sigmoid(tf.matmul(y_7, W_8) + b_8)
 
 #Get the function to minimize
 loss = tf.nn.l2_loss(y - x)
@@ -35,7 +55,7 @@ loss = tf.nn.l2_loss(y - x)
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
 l2diff = tf.sqrt( tf.reduce_sum(tf.square(tf.subtract(x, y)), reduction_indices=1))
-loss_mean = tf.reduce_mean(loss)
+loss_mean = tf.reduce_mean(l2diff)
 
 with tf.Session() as sess:
 	#initialize our Variables
